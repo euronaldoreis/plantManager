34200e2c13ce0ec129880ad2dd102e2efae89035
Ronaldo Reis <reeisfst@gmail.com>
2021-07-04 16:05:41 -0400

    Add and config axios and server-json

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { 
     View, 
     Text,
@@ -8,11 +8,34 @@ import {
 import { EnviromentButton } from '../components/EnviromentButton';
 
 import {Header} from '../components/Header'
+import api from '../services/api';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface EnviromentProps {
+    key: string;
+    title: string;
+}
+
 export function PlantSelect(){
+    const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
+
+
+    useEffect(() =>{
+        async function fetchEnviroment(){
+            const { data } = await api.get('plants_environments');
+            setEnviroments([
+                {
+                    key: 'all',
+                    title: 'Todos',
+                },
+                ...data
+            ])
+        }
+        fetchEnviroment();
+    }, [])
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -26,11 +49,10 @@ export function PlantSelect(){
             </View>
             <View>
                 <FlatList
-                    data={[1,2,3,4,5]}
+                    data={enviroments}
                     renderItem={({ item }) => (
                         <EnviromentButton 
-                            title="Cozinha" 
-                            active
+                            title={item.title}
                         />
                     )}
                     horizontal
