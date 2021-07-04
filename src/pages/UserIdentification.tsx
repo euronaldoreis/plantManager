783065816750bbded382b009cc1bfa18ee669ba8
Ronaldo Reis <reeisfst@gmail.com>
2021-07-04 13:47:12 -0400

    Add keyboard dismess

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -6,7 +6,9 @@ import {
     View,
     TextInput,
     KeyboardAvoidingView,
-    Platform
+    TouchableWithoutFeedback,
+    Platform,
+    Keyboard
 } from 'react-native';
 
 import {Button} from '../components/Button';
@@ -47,36 +49,38 @@ export function UserIdentification(){
                 style={styles.container}
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             >
-                <View style={styles.content}>
-                    <View style={styles.form}>
-                        <View style={styles.header}>
-                            <Text style={styles.emoji}>
-                                { isFilled ? '😁' : '😀'}
-                            </Text>
-                            <Text style={styles.title}>
-                                Como podemos{'\n'}
-                                chamar você?
-                            </Text>
-                        </View>
-                        <TextInput
-                            style={[
-                                styles.input,
-                                (isFocused || isFilled ) && 
-                                {borderColor: colors.green}
-                            ]}
-                            placeholder="Digite um nome"
-                            onBlur={handleInputBlur}
-                            onFocus={handleInputFocus}
-                            onChangeText={handleInputChange}
-                        />
-                        <View style={styles.footer}>
-                            <Button 
-                                title="Confirmar"
-                                onPress={handleSubmit}
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <View style={styles.header}>
+                                <Text style={styles.emoji}>
+                                    { isFilled ? '😁' : '😀'}
+                                </Text>
+                                <Text style={styles.title}>
+                                    Como podemos{'\n'}
+                                    chamar você?
+                                </Text>
+                            </View>
+                            <TextInput
+                                style={[
+                                    styles.input,
+                                    (isFocused || isFilled ) && 
+                                    {borderColor: colors.green}
+                                ]}
+                                placeholder="Digite um nome"
+                                onBlur={handleInputBlur}
+                                onFocus={handleInputFocus}
+                                onChangeText={handleInputChange}
                             />
+                            <View style={styles.footer}>
+                                <Button 
+                                    title="Confirmar"
+                                    onPress={handleSubmit}
+                                />
+                            </View>
                         </View>
                     </View>
-                </View>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
