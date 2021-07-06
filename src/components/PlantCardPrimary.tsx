2790af78149620a384f26f7b1286c2e1ea55b74a
Ronaldo Reis <reeisfst@gmail.com>
2021-07-05 22:12:13 -0400

    add: using svg with react-native-svg

--- src/components/PlantCardPrimary.tsx
+++ src/components/PlantCardPrimary.tsx
@@ -4,6 +4,8 @@ import {
     Text
 } from 'react-native';
 import {RectButton, RectButtonProps} from 'react-native-gesture-handler';
+import { SvgFromUri } from 'react-native-svg';
+
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
@@ -20,6 +22,11 @@ export const PlantCardPrimary = ({data, ...rest}: PlantProps) => {
             style={styles.container}
             {...rest}
         >
+            <SvgFromUri 
+                uri={data.photo} 
+                width={70} 
+                height={70}
+            />
             <Text style={styles.text}>
                 {data.name}
             </Text>
