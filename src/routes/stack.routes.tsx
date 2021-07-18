efbfc9e8fafb0599655d8c26081b9e071caa6812
Ronaldo Reis <reeisfst@gmail.com>
2021-07-18 16:58:20 -0400

    add: bottomTabs

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,10 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect';
 
 import colors from '../styles/colors';
 import { PlantSave } from '../pages/PlantSave';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -34,11 +34,15 @@ const AppRoutes: React.FC = () => (
         />
         <stackRoutes.Screen
             name="PlantSelect"
-            component={PlantSelect}
+            component={AuthRoutes}
         />
         <stackRoutes.Screen
             name="PlantSave"
             component={PlantSave}
+        />
+        <stackRoutes.Screen
+            name="MyPlants"
+            component={AuthRoutes}
         />
     </stackRoutes.Navigator>
 )
